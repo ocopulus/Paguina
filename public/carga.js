795c201234fe8c6ae02eaebfb2678ea4e61b4960
ocopulus <jlemus003@gmail.com>
2018-04-19 05:54:51 -0600

    arreglo de actualizacion automatica

--- public/carga.js
+++ public/carga.js
@@ -1,29 +1,34 @@
 
-$('#tweets').html('yolo');
-$('#tweets').append('yolo2');
+$('#tweets').html('Cargando Datos Papu ;V');
 
-$.get( '/tweets',{}, function(data) {
-    texto  = "";
-    data.forEach(function(element) {
-      div = '<div class="alert alert-secondary" role="alert">' +
-            element['user'] + ' @ ' + element['message'] +
-            '</div>';
-      texto += div;
-    });
-    $('#tweets').html(texto);
-
-    Numero_tweets = data.length;
-    usuarios = new Array();
-    Numero_usuarios = 0;
-    data.forEach(function(element){
-      exite = usuarios.find(function(ele){
-        return ele == element['user'];
+function pedirDatos(){
+  $.get( '/tweets',{}, function(data) {
+      texto  = "";
+      data.forEach(function(element) {
+        div = '<div class="alert alert-secondary" role="alert">' +
+              element['user'] + ' @ ' + element['message'] +
+              '</div>';
+        texto += div;
       });
-      if(exite != element['user']){
-        Numero_usuarios++;
-      }
-    });
-    $('#usuario').html('Numero de Usuarios: ' + Numero_usuarios);
-    $('#numtweet').html('Total de Tweets: ' + Numero_tweets);
-    console.log(Numero_usuarios);
-});
+      $('#tweets').html(texto);
+
+      Numero_tweets = data.length;
+      usuarios = new Array();
+      Numero_usuarios = 0;
+      data.forEach(function(element){
+        exite = usuarios.find(function(ele){
+          return ele == element['user'];
+        });
+        if(exite != element['user']){
+          Numero_usuarios++;
+        }
+      });
+      $('#usuario').html('Numero de Usuarios: ' + Numero_usuarios);
+      $('#numtweet').html('Total de Tweets: ' + Numero_tweets);
+      console.log(Numero_usuarios);
+  });
+}
+
+window.setInterval(function(){
+	pedirDatos();
+},5000);
